41ee45829007288233347f8ba6c92940c9983e33
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-01-11 21:43:46 -0300

    User class created
    User class create with your's atributes.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,66 @@
 package model;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	private Integer Id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+
+	public User(Integer id, String name, String email, String password) {
+		super();
+		Id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
 }
